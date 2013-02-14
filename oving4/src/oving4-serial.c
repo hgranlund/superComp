8c750a30f7a74d759b7570671f1d0b1edbeec8fd
Simen Haugerud Granlund <simen@hgranlund.com>
2013-02-14 14:36:05 +0100

    startes on oving4

---
+++ oving4/src/oving4-serial.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "common.h"
+
+
+
+// calculates \sum_K v_i'*A*v_i
+Vector genVec(int n)
+{
+    Vector vec = createVector(n);
+    for (int i = 0; i < n; ++i)
+    {
+      vec->data[i]=1/((i+1)*(i+1));
+      printf("i %d\n",i);
+    }
+    return vec;
+}
+
+double doSum(Vector vec){
+  double sum=0;
+
+  for (int i = 0; i < vec->len; ++i)
+  {
+      sum = sum +vec->data[i];
+      printf("sum ++ %d\n", sum);
+  }
+  return sum;
+}
+
+int main(int argc, char** argv)
+{
+  if (argc < 2) {
+    printf("need one parameters, length of vector\n");
+    return 1;
+  }
+  int N=atoi(argv[1]);
+
+
+
+
+  double time = WallTime();
+  Vector vec = genVec(N);
+  double sum = doSum(vec);
+
+  printf("sum: %f\n", sum);
+  printf("elapsed: %f\n", WallTime()-time);
+
+  freeMatrix(vec);
+  return 0;
+}
